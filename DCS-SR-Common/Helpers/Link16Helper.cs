a04337e0d57ee8758e632e1ef47f180e2bd707d5
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-08-23 15:44:23 -0400

    Update Link16Helper.cs

--- DCS-SR-Common/Helpers/Link16Helper.cs
+++ DCS-SR-Common/Helpers/Link16Helper.cs
@@ -2,17 +2,23 @@
 {
 	public class Link16Helper
 	{
-		public static readonly double link16BaseFreq = 1030.0 * 1000000; //Start at UHF 300
-		public static readonly double link16FreqSeperation = 1.0 * 100000; //0.1 MHZ between MIDS channels
+		public static readonly double BaseFreq = 1030.0 * 1000000; //Start at UHF 300
+		public static readonly double FreqSeperation = 1.0 * 100000; //0.1 MHZ between MIDS channels
 
+		public static readonly int MinimumChannel = 0;
+		public static readonly int MaximumChannel = 126;
+
+		public static readonly double MinimumFreq = Link16ToFrequency(MinimumChannel);
+		public static readonly double MaximumFreq = Link16ToFrequency(MaximumChannel);
+		
 		public static double Link16ToFrequency(int channel)
 		{
-			return (link16BaseFreq + (link16FreqSeperation * channel));
+			return (BaseFreq + (FreqSeperation * channel));
 		}
         
 		public static int FrequencyToLink16(double frequency)
 		{
-			return (int)((frequency - link16BaseFreq) / link16FreqSeperation);
+			return (int)((frequency - BaseFreq) / FreqSeperation);
 		}
 	}
 }
