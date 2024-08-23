a673643c392e3e142258535a921bcf82cd2cc875
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-08-23 12:47:11 -0400

    Create Link16 Helper

---
+++ DCS-SR-Common/Helpers/Link16Helper.cs
@@ -0,0 +1,18 @@
+﻿namespace Ciribob.DCS.SimpleRadio.Standalone.Common
+{
+	public class Link16Helper
+	{
+		public static readonly double link16BaseFreq = 1030.0 * 1000000; //Start at UHF 300
+		public static readonly double link16FreqSeperation = 1.0 * 100000; //0.1 MHZ between MIDS channels
+
+		public static double Link16ToFrequency(int channel)
+		{
+			return (link16BaseFreq + (link16FreqSeperation * channel));
+		}
+        
+		public static int FrequencyToLink16(double frequency)
+		{
+			return (int)((frequency - link16BaseFreq) / link16FreqSeperation);
+		}
+	}
+}
