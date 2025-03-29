277ac587e267e2ef0ae3bb8e3feac73067baa888
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-03-29 16:02:41 -0400

    Checkpoint

---
+++ AvaloniaApplication1/Views/RadioOverlay/IntercomControl.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplication1.Views.RadioOverlay;
+
+public partial class IntercomControl : UserControl
+{
+	public IntercomControl()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AvaloniaApplication1/Views/RadioOverlay/RadioControl.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplication1.Views.RadioOverlay;
+
+public partial class RadioControl : UserControl
+{
+	public RadioControl()
+	{
+		InitializeComponent();
+	}
+}
