a2af4f7f7a4618dbc763430c0fc858b86eb9cb79
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-06-13 20:47:26 -0400

    Import Previous Work

---
+++ Client/Client/Views/GeneralPanel.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Client.Views;
+
+public partial class GeneralPanel : UserControl
+{
+	public GeneralPanel()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Client/Client/Views/StatusBar.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Client.Views;
+
+public partial class StatusBar : UserControl
+{	
+	public StatusBar()
+	{
+		InitializeComponent();
+	}
+}
