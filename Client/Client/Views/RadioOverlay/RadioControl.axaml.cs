f3529da20492e20a451802d9055da8059a9fcb89
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-06-13 21:00:03 -0400

    Add Radio Overlay Controls

---
+++ Client/Client/Views/RadioOverlay/RadioControl.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Client.Views.Radio_Overlay;
+
+public partial class RadioControl : UserControl
+{
+	public RadioControl()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Client/Client/Views/RadioOverlay/RadioOverlay.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Client.Views.RadioOverlay;
+
+public partial class RadioOverlay : UserControl
+{
+	public RadioOverlay()
+	{
+		InitializeComponent();
+	}
+}
