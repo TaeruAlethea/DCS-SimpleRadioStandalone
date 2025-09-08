b6e774622e7cc53aae120660466b348aea1c6401
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-09-07 20:52:25 -0400

    Init new project

---
+++ Client/Client/App.axaml.cs
@@ -0,0 +1,51 @@
+using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using Client.ViewModels;
+using Client.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client;
+
+public partial class App : Application
+{
+	public override void Initialize()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+
+	public override void OnFrameworkInitializationCompleted()
+	{
+		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+		{
+			// Line below is needed to remove Avalonia data validation.
+			// Without this line you will get duplicate validations from both Avalonia and CT
+			BindingPlugins.DataValidators.RemoveAt(0);
+			desktop.MainWindow = new MainWindow
+			{
+				DataContext = new MainViewModel()
+			};
+		}
+		else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+		{
+			singleViewPlatform.MainView = new MainView
+			{
+				DataContext = new MainViewModel()
+			};
+		}
+
+		base.OnFrameworkInitializationCompleted();
+	}
+
+	public static IServiceProvider ConfigureServices()
+	{
+		var services = new ServiceCollection();
+		
+		services.AddSingleton<MainViewModel>();
+		
+		return services.BuildServiceProvider();
+	}
+}
